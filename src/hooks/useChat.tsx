1c3f5c0602924504cff26ff2bafeab7b09071cb9
Aryaveer <aryaveer1214@gmail.com>
2026-07-17 23:37:40 +0530

    feat: add streaming.

--- src/hooks/useChat.tsx
+++ src/hooks/useChat.tsx
@@ -1,78 +1,81 @@
 import type { Message } from "../types";
 import { useState } from "react";
+import { fetchEventSource } from "@microsoft/fetch-event-source";
 
 const useChat = () => {
-    const [messages, setMessages] = useState<Message[]>([]);
-    const [input, setInput] = useState("");
-    const [loading, setIsLoading] = useState(false);
-    const [model, setModel] = useState("llama-3.1-8b-instant");
+  const [messages, setMessages] = useState<Message[]>([]);
+  const [input, setInput] = useState("");
+  const [loading, setIsLoading] = useState(false);
+  const [model, setModel] = useState("llama-3.1-8b-instant");
 
-    const addMessage = async (message: string) => {
+  const addMessage = async (message: string) => {
+    try {
+      const userMessage: Message = {
+        id: crypto.randomUUID(),
+        role: "user",
+        content: message,
+      };
 
-        try {
-            // It will update the messages and will send the messages to the backend as well.
-            const userMessage: Message = {
-                id: crypto.randomUUID(),
-                role: "user",
-                content: message,
-            }
+      const updatedMessages = [...messages, userMessage];
 
-            const updatedMessages = [...messages, userMessage];
+      setMessages(updatedMessages);
+      setIsLoading(true);
+      setInput("");
 
-            setMessages(updatedMessages);
+      await fetchEventSource("http://localhost:3000/chat-stream", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          messages: updatedMessages,
+          model: model,
+        }),
+        onmessage(event: any) {
+          const data = JSON.parse(event.data);
+          if (data.content) {
+            setMessages((prevMessages) => {
+              const lastMsg = prevMessages[prevMessages.length - 1];
 
-            setIsLoading(true);
+              if (lastMsg.role !== "assistant") {
+                return [
+                  ...prevMessages,
+                  {
+                    id: crypto.randomUUID(),
+                    role: "assistant",
+                    content: data.content,
+                  },
+                ];
+              } else {
+                return [
+                  ...prevMessages.slice(0, -1),
+                  {
+                    ...lastMsg,
+                    content: lastMsg.content + data.content,
+                  },
+                ];
+              }
+            });
+          }
+        },
+      });
 
-            setInput("");
-
-            const api_response = await fetch("http://localhost:3000/chat", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify(
-                    { 
-                        messages: updatedMessages,
-                        model: model
-                    }
-                ),
-            })
-
-            if (!api_response.ok) {
-                throw new Error("Failed to send message");
-            }
-
-            const response  = await api_response.json();
-
-            const assistantMessage: Message = {
-                id: crypto.randomUUID(),
-                role: "assistant",
-                content: response.response
-            }
-
-            setMessages([...updatedMessages, assistantMessage]);
-
-            setIsLoading(false);
-
-        } catch (error) {
-            console.log("Error sending message:", error);
-
-            setIsLoading(false);
-        }
-        
-        
+      setIsLoading(false);
+    } catch (error) {
+      console.log("Error sending message:", error);
+      setIsLoading(false);
     }
+  };
 
-    return {
-        messages,
-        input,
-        setInput,
-        loading,
-        addMessage,
-        model,
-        setModel
-    }
-
-}
+  return {
+    messages,
+    input,
+    setInput,
+    loading,
+    addMessage,
+    model,
+    setModel,
+  };
+};
 
 export default useChat;
